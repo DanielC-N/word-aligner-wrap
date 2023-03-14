f685da120d06354d5b2a78dbb8da15e46f3495d3
danielc-n <npmdanielc-n@protonmail.com>
2023-03-14 15:44:07 +0100

    removing components (useless) and and more template code

--- src/utils/alignmentApi.js
+++ src/utils/alignmentApi.js
@@ -1,30 +1,63 @@
 import { isNull } from 'lodash';
-import { Proskomma } from 'proskomma-cross';
+// import { Proskomma } from 'proskomma-cross';
+import ProskommaInterface from '../classes/ProskommaInterface';
 import Epitelete from 'epitelete';
 import { PipelineHandler } from 'proskomma-json-tools';
 
 export class Aligner {
     /**
      *
-     * @param {Proskomma} proskomma - a proskomma instance
+     * @param {String[]} sourceUsfms - source raw usfm, code lang and abbr
+     * @param {String[]} targetUsfm - target raw usfm, code lang and abbr
      * @param {boolean} verbose
      */
-    constructor({proskomma = null, verbose=false}) {
+    constructor({sourceUsfm=[], targetUsfm=[], verbose=false}) {
 
-        if (proskomma !== null) {
-            this.proskomma = proskomma;
-        } else {
-            this.proskomma = new Proskomma();
-        }
-        const query = '{ id }';
-        const content = proskomma.gqlQuerySync(query) || {};
+        this.proskommaInterface = new ProskommaInterface();
+        this.proskommaInterface.addRawDocument(sourceUsfm[0], sourceUsfm[1], sourceUsfm[2]);
+        this.proskommaInterface.addRawDocument(targetUsfm[0], targetUsfm[1], targetUsfm[2]);
+        
+        this.currentSentence = [];
+        this.sourceUsfm = sourceUsfm[0];
+        this.targetUsfm = targetUsfm[0];
+        this.verbose = verbose;
+    }
 
-        if (!isNull(this.proskomma) || !content || !content.data.id) {
-            throw new Error("Error instanciating Proskomma, please contact Jesus, he's the only one capable of resolving this problem");
+    /**
+     * 
+     * @returns {string[]}
+     */
+    getCurrentSentence() {
+        return this.currentSentence;
+    }
+
+    /**
+     * 
+     * @param {string[]|string} sentence the sentence for alignment
+     */
+    setCurrentSentence(sentence) {
+        if(typeof sentence === "object" && !isNull(sentence[0]) && typeof sentence[0] === "string") {
+            this.currentSentence = sentence;
+        } else if (typeof sentence === "string") {
+            this.currentSentence = sentence.trim().split(/\W+/g);
         }
     }
 
+    /**
+     * Get a well formated JSON word alignment informations
+     * @returns {JSON}
+     */
+    getAlignmentJSON() {
+        // TODO
+    }
 
+    addAlignment() {
+        // TODO
+    }
+
+    removeAlignment(indexA, IndexB) {
+        // TODO
+    }
 }
 
 /**
